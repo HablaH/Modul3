4ecf9a578222d682c0709934c7700f41f6572a99
Harald S <harald@getacademy.no>
2021-01-04 17:11:58 +0100

    tipping 1 kamp

---
+++ div solo oppgaver/Tippekupong/Tippekupong - en kamp/Tippekupong - en kamp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tippekupong___en_kamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nHva har du tippet for denne kampen? ");
+            var bet = Console.ReadLine();
+            var match = new Match(bet);
+            while (match.IsRunning)
+            {
+                Console.Write("Kommandoer: \r\n - H = scoring hjemmelag\r\n - B = scoring bortelag\r\n - X = kampen er ferdig\r\nAngi kommando: ");
+                var command = Console.ReadLine();
+                if (command == "X") match.Stop();
+                else if (command == "H" || command == "B") match.AddGoal(command == "H");
+                Console.WriteLine($"Stillingen er {match.GetScore()}.");
+            }
+
+            var isBetCorrectText = match.IsBetCorrect() ? "riktig" : "feil";
+            Console.WriteLine($"Du tippet {isBetCorrectText}");
+        }
+    }
+}
