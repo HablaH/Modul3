ae2f23558747fd501fd87adda3c3e1784c6a018f
Harald S <harald@getacademy.no>
2021-01-06 14:25:02 +0100

    12 Matches ferdig

---
+++ div solo oppgaver/Tippekupong/Tippekupong 12 kamper/Tippekupong 12 kamper/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tippekupong_12_kamper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nSkriv inn dine 12 tips med komma mellom: ");
+            var bets = Console.ReadLine();
+            var twelveMatches = new TwelveMatches(bets);
+
+
+
+            while (true)
+            {
+                Console.Write("Skriv kampnr. 1-12 for scoring eller X for alle kampene er ferdige\r\nAngi kommando: ");
+                var command = Console.ReadLine();
+                if (command == "X") break;
+                else twelveMatches.SelectMatch(command);
+
+                var team = Console.ReadLine();
+                twelveMatches.AddGoal(team == "H");
+
+                twelveMatches.GetScore();
+            }
+        }
+    }
+}
---
+++ div solo oppgaver/Tippekupong/Tippekupong 12 kamper/Tippekupong 12 kamper/TwelveMatches.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tippekupong_12_kamper
+{
+    class TwelveMatches
+    {
+        private readonly string[] _bets;
+        private Match[] _matches = new Match[12];
+        private Match _selectedMatch;
+        private int _correctCount = 0;
+        private int _matchNo;
+
+        public TwelveMatches(string betsText)
+        {
+            _bets = betsText.Split(',');
+            
+            for (var i = 0; i < 12; i++)
+            {
+                _matches[i] = new Match(_bets[i]);
+            }
+        }
+
+        public void SelectMatch(string command)
+        {
+            _matchNo = Convert.ToInt32(command);
+            Console.Write($"Scoring i kamp {_matchNo}. \r\nSkriv H for hjemmelag eller B for bortelag: ");            
+            var selectedIndex = _matchNo - 1;
+            _selectedMatch = _matches[selectedIndex];
+        }
+
+        public void AddGoal(bool isHomeTeam)
+        {
+            _selectedMatch.AddGoal(isHomeTeam);
+        }
+
+        public void GetScore()
+        {
+            for (var index = 0; index < _matches.Length; index++)
+            {
+                var match = _matches[index];
+                _matchNo = index + 1;
+                var isBetCorrect = match.IsBetCorrect();
+                var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
+                if (isBetCorrect) _correctCount++;
+                Console.WriteLine($"Kamp {_matchNo}: {match.GetScore()} - {isBetCorrectText}");
+            }
+
+            Console.WriteLine($"Du har {_correctCount} rette.");
+        }
+    }
+}
