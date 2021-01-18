291e1e643be2f6d07b91b131961a1212e8a1b66f
Harald S <harald@getacademy.no>
2021-01-18 20:12:47 +0100

    added tekst returns

--- div solo oppgaver/SimuleringFlaskeGåte/SimuleringFlaskeGåte/SimuleringFlaskeGåte/Program.cs
+++ div solo oppgaver/SimuleringFlaskeGåte/SimuleringFlaskeGåte/SimuleringFlaskeGåte/Program.cs
@@ -1,20 +1,35 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Globalization;
 using System.Text;
 
 namespace SimuleringFlaskeGåte
 {
     class Program
     {
+        //cd C:\Users\HablaH\Documents\Koding\Github\Modul3\div solo oppgaver\SimuleringFlaskeGåte\SimuleringFlaskeGåte\SimuleringFlaskeGåte\bin\Debug
+        //SimuleringFlaskeGåte 
         static void Main(string[] args)
         {
-            var bottle1 = new Bottle(5);
-            var bottle2 = new Bottle(7);
+            Bottle bottle1 = new Bottle(int.Parse(args[0]));
+            Bottle bottle2 = new Bottle(int.Parse(args[1]));
+            int wantedVolume = int.Parse(args[2]);
+            int numberOfOperations = int.Parse(args[3]);
+
+            Console.WriteLine($"flaske 1: {bottle1.Capacity} Liter  \r\n" +
+                              $"flaske 2: {bottle2.Capacity} Liter  \r\n" +
+                              $"ønket mengde : {wantedVolume} Liter \r\n" +
+                              $"antall lovlige operasjoner: {numberOfOperations}");
+
+            
+            //var bottle1 = new Bottle(5);
+            //var bottle2 = new Bottle(7);
             //int[] ops = new[] { 0,2,0,4,7,2,0,4};
             //DoOperations(ops, bottle1, bottle2);
             //Console.WriteLine($"1: {bottle1.Content}  2: {bottle2.Content}");
             //CheckIfSolvedAndExitApplicationIfSo(bottle1, bottle2, 6, ops);
 
-            TryWithGivenNumberOfOperations(10, bottle1, bottle2, 3);
+            TryWithGivenNumberOfOperations(numberOfOperations, bottle1, bottle2, wantedVolume);
         }
 
         private static void TryWithGivenNumberOfOperations(int numberOfOperations, Bottle bottle1, Bottle bottle2, int wantedVolume)
@@ -34,21 +49,16 @@ namespace SimuleringFlaskeGåte
         {
             bottle1.Empty();
             bottle2.Empty();
-            for (int i= 0; i < operations.Length; i++)
+            foreach (var operation in operations)
             {
-                int operationNo = operations[i];
-                if (operationNo == 0) continue;
-                else if (operationNo == 1) bottle1.FillToTopFromTap(); // Fylle flaske 1 fra springen
-                else if (operationNo == 2) bottle2.FillToTopFromTap(); // Fylle flaske 2 fra springen
-                else if (operationNo == 3) bottle2.Fill(bottle1.Empty()); // Tømme flaske 1 i flaske 2 - 
-                // Tanken er at Empty() returnerer hvor mye det var i flasken før den ble tømt
-                else if (operationNo == 4) bottle1.Fill(bottle2.Empty()); // Tømme flaske 2 i flaske 1
-                else if (operationNo == 5) bottle2.FillToTop(bottle1); // Fylle opp flaske 2 med flaske 1
-                // Tanken er at FillToTop tar en annen Bottle som parameter. Hvis det er nok, fyller den 
-                // bottle2 og reduserer bottle1 tilsvarende. Hvis ikke gjør den ingenting.
-                else if (operationNo == 6) bottle1.FillToTop(bottle2); // Fylle opp flaske 1 med flaske 2
-                else if (operationNo == 7) bottle1.Empty(); // Tømme flaske 1 (kaste vannet)
-                else if (operationNo == 8) bottle2.Empty(); // Tømme flaske 2 (kaste vannet)
+                if (operation == 1) bottle1.FillToTopFromTap();                 // Fylle flaske 1 fra springen
+                else if (operation == 2) bottle2.FillToTopFromTap();            // Fylle flaske 2 fra springen
+                else if (operation == 3) bottle2.Fill(bottle1.Empty());   // Tømme flaske 1 i flaske 2
+                else if (operation == 4) bottle1.Fill(bottle2.Empty());   // Tømme flaske 2 i flaske 1
+                else if (operation == 5) bottle2.FillToTop(bottle1);            // Fylle opp flaske 2 med flaske 1
+                else if (operation == 6) bottle1.FillToTop(bottle2);            // Fylle opp flaske 1 med flaske 2
+                else if (operation == 7) bottle1.Empty();                       // Tømme flaske 1 (kaste vannet)
+                else if (operation == 8) bottle2.Empty();                       // Tømme flaske 2 (kaste vannet)
             }
         }
 
@@ -62,9 +72,26 @@ namespace SimuleringFlaskeGåte
                 {
                         operationsText.Append(operation.ToString() + " - ");
                 }
-                Console.WriteLine($"Fant ønsket volum ved operasjon {operationsText.ToString()}" );
+                Console.WriteLine($"Fant ønsket volum ved å: \r\n{OperationsToText(operations)}" );
                 Environment.Exit(-1);
             }
+        }
+
+        private static string OperationsToText(int[] operations)
+        {
+            StringBuilder operationsText = new StringBuilder();
+            foreach (var operation in operations)
+            {
+                if      (operation == 1) operationsText.Append($"({operation.ToString()}) Fylle flaske 1 \r\n");
+                else if (operation == 2) operationsText.Append($"({operation.ToString()}) Fylle flaske 2 \r\n");
+                else if (operation == 3) operationsText.Append($"({operation.ToString()}) Tømme flaske 1 i flaske 2 \r\n");
+                else if (operation == 4) operationsText.Append($"({operation.ToString()}) Tømme flaske 2 i flaske 1 \r\n");
+                else if (operation == 5) operationsText.Append($"({operation.ToString()}) Fylle opp flaske 2 med flaske 1 \r\n");
+                else if (operation == 6) operationsText.Append($"({operation.ToString()}) Fylle opp flaske 1 med flaske 2 \r\n");
+                else if (operation == 7) operationsText.Append($"({operation.ToString()}) Tømme flaske 1 \r\n");
+                else if (operation == 8) operationsText.Append($"({operation.ToString()}) Tømme flaske 2 \r\n");
+            }
+            return operationsText.ToString();
         }
 
         private static bool UpdateOperations(int[] operations)
@@ -72,7 +99,7 @@ namespace SimuleringFlaskeGåte
             int i;
             for (i = operations.Length - 1; i >= 0; i--)
             {
-                if (operations[i] < 7)
+                if (operations[i] < 8)
                 {
                     operations[i]++;
                     break;
