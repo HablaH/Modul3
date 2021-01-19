4fed48b88708bff84daeba4b05520c7deaccc371
Harald S <harald@getacademy.no>
2021-01-19 19:51:27 +0100

    + test file

---
+++ div solo oppgaver/SimuleringFlaskeGåte/SimuleringFlaskeGåte/SimuleringFlaskeGåteTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimuleringFlaskeGåteTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimuleringFlaskeGåteTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d2ba8ea3-4a17-4300-a7f2-b9d5438571b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
